c0412d9e4fde0b89ce62ae8b5680967bd2a1d6e1
ama29n <ama3149n@gmail.com>
2023-12-03 20:19:37 +0530

    Responsive 2

--- src/components/Footer/DontMissOut/DontMissOut.jsx
+++ src/components/Footer/DontMissOut/DontMissOut.jsx
@@ -6,7 +6,9 @@ const DontMissOut = () => {
   return (
     <div className={styles.box}>
       <div className={styles.leftBox}>
-        <HeadingBefore emoji="💁‍♂️" text="Don't miss out" />
+        <div className={styles.headingBefore}>
+          <HeadingBefore emoji="💁‍♂️" text="Don't missout" />
+        </div>
         <div className={styles.heading}>
           Unleash your <span className="between-text-orange">Potential</span>{" "}
           with us
@@ -15,7 +17,7 @@ const DontMissOut = () => {
           Join our community of ambitious individuals and organizations eager to
           make a difference.
         </div>
-        <PrimaryButton text="Try Out Now" />
+        <PrimaryButton text="Try Out" />
       </div>
       <div className={styles.rightBox}>
         <div className={styles.image}>
